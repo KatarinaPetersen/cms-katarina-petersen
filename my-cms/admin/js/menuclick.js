95d706db04b6089da087f982df865e9e7184404f
KatarinaPetersen <8555@rts-365.dk>
2017-11-23 14:34:21 +0100

    op

--- my-cms/admin/js/menuclick.js
+++ my-cms/admin/js/menuclick.js
@@ -12,47 +12,38 @@
         caller.classList.toggle('itemActive');
 
         switch (caller.dataset.cmd) {
-            case 'logout': // OK
-                logout();
-                break;
-            case 'categories': // OK 
-                categories();
-                break;
             case 'catEdit': // OK
                 catEdit(caller); //caller fortæller hvad der bliver kaldet på
                 break;
             case 'catAdd':
                 catAdd(caller);
                 break;
-            case 'catDelete' :
+            case 'catDelete':
                 catDelete(caller);
+                break;
+            case 'categories': // OK 
+                categories();
+                break;
+            case 'userEdit': // OK
+                userEdit(caller);
+                break;
+            case 'userAdd':
+                userAdd(caller);
+                break;
+            case 'userDelete':
+                userDelete(caller);
+                break;
+            case 'users':
+                users()
                 break;
             case 'article':
                 break
-            case 'users':
-                alert(ds.cmd)
+            case 'logout': // OK
+                logout();
                 break;
             default:
                 alert(caller);
         }
-    }
-
-    function catDelete(caller){
-        var formId = caller.dataset.id;
-        var form = document.querySelector(`#${formId}`);
-        var formData = new FormData(form);
-        fetch('/menuitems',{
-            method : 'delete', 
-            credentials: 'include',
-            body : formData
-        })
-        .then(function (data) {
-            document.querySelector('div[data-cmd="categories"]').click();
-        })        
-        .catch(function(err){
-            console.log(err);
-        });
-     
     }
 
     function catEdit(caller) {
@@ -70,12 +61,40 @@
             })
     }
 
-    function logout() {
-        fetch('/logout', { credentials: 'include', method: 'delete' })
-            .then(function () {
-                document.querySelector("#title").innerHTML = 'Du loggede af...';
-                setTimeout(function () { location.href = "/"; }, 1000);
+    function catAdd(caller) {
+        var form = document.querySelector('#frmCatAdd');
+        // lav en kopi af formularen til at sende afsted. Derved undgåes at siden refreshes hvis den originale form sendes
+        var formData = new FormData(form); fetch('/menuitems', {
+            method: 'post',
+            credentials: 'include',
+            body: formData
+        })
+            .then(function (data) {
+                // tager fat i knappen KATEGORIER og simulerer et klik, så siden genindlæses og tabellen refreshes
+                document.querySelector('div[data-cmd="categories"]').click();
+            })
+            .catch(function (err) {
+                console.log(err);
             });
+
+    }
+
+    function catDelete(caller) {
+        var formId = caller.dataset.id;
+        var form = document.querySelector(`#${formId}`);
+        var formData = new FormData(form);
+        fetch('/menuitems', {
+            method: 'delete',
+            credentials: 'include',
+            body: formData
+        })
+            .then(function (data) {
+                document.querySelector('div[data-cmd="categories"]').click();
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+
     }
 
     function categories() {
@@ -132,22 +151,115 @@
             })
     }
 
-    function catAdd(caller){
-        var form = document.querySelector('#frmCatAdd');
-        // lav en kopi af formularen til at sende afsted. Derved undgåes at siden refreshes hvis den originale form sendes
-        var formData = new FormData(form);    fetch('/menuitems',{
-            method : 'post', 
+    function userEdit(caller) {
+        var formId = caller.dataset.id
+        var frm = document.querySelector(`#${formId}`);
+        var frmData = new FormData(frm);
+        fetch('/useritems', {
             credentials: 'include',
-            body : formData
+            method: 'put',
+            body: frmData
         })
-        .then(function (data) {
-            // tager fat i knappen KATEGORIER og simulerer et klik, så siden genindlæses og tabellen refreshes
-            document.querySelector('div[data-cmd="categories"]').click();
-        })        
-        .catch(function(err){
-            console.log(err);
-        });
-        
+            .then(function (data) {
+                document.querySelector('div[data-cmd="users"]').click();
+                // return data.json();
+            })
+    }
+
+    function userAdd(caller) {
+        var form = document.querySelector('#frmUserAdd');
+        // lav en kopi af formularen til at sende afsted. Derved undgåes at siden refreshes hvis den originale form sendes
+        var formData = new FormData(form); fetch('/useritems', {
+            method: 'post',
+            credentials: 'include',
+            body: formData
+        })
+            .then(function (data) {
+                // tager fat i knappen BRUGERE og simulerer et klik, så siden genindlæses og tabellen refreshes
+                document.querySelector('div[data-cmd="users"]').click();
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+
+    function userDelete(caller) {
+        var formId = caller.dataset.id;
+        var form = document.querySelector(`#${formId}`);
+        var formData = new FormData(form);
+        fetch('/useritems', {
+            method: 'delete',
+            credentials: 'include',
+            body: formData
+        })
+            .then(function (data) {
+                document.querySelector('div[data-cmd="users"]').click();
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+
+    function users() {
+        fetch('/useritems', { method: 'get' })
+            .then(function (data) {
+                return data.json();
+            })
+            .then(function (jsonData) {
+                var content = `
+                            <div class="cat-container"><div class="cat-head">Rediger brugere</div>
+                                <form>
+                                    <div class="cat-row">
+                                        <div class="cat-cell"><input readonly type="text" value="Brugernavn"></div>
+                                        <div class="cat-cell"><input readonly type="text" value="Password"></div>
+                                    </div>
+                                </form>`;
+                jsonData.forEach(function (d) {
+                    content += `<form id="frm${d.id}">
+                                    <div class="cat-row">
+                                        <div class="cat-cell">
+                                            <input name="id" type="hidden" value="${d.id}">
+                                            <input name="username" type="text" value="${d.username}">
+                                        </div>
+                                        <div class="cat-cell">
+                                            <input name="userpw" type="text" value="${d.password}">
+                                        </div>
+                                        <div class="cat-cell">
+                                            <img data-cmd="userEdit" data-id="frm${d.id}" class="iconImage clickable" src="img/Refresh.png" title="Opdater">
+                                        </div>
+                                        <div class="cat-cell">
+                                            <img data-cmd="userDelete" data-id="frm${d.id}" class="iconImage clickable" src="img/Trash.png" title="Slet">
+                                        </div>
+                                    </div>
+                                </form>`;
+                });
+                content += `<br><hr>Eller tilføj<form id="frmUserAdd">
+                                <div class="cat-row">
+                                    <div class="cat-cell">
+                                        <input name="username" type="text" placeholder="Brugernavn">
+                                    </div>
+                                    <div class="cat-cell">
+                                        <input name="userpw" type="text" placeholder="Password">
+                                    </div>
+                                    <div class="cat-cell">
+                                        <img data-cmd="userAdd"  class="iconImage clickable" src="img/plus-2x.png" title="Opdater">
+                                    </div>
+                                </div>
+                            <form>`;
+                content += `</div>`;
+                document.querySelector('#content').innerHTML = content;
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    }
+
+    function logout() {
+        fetch('/logout', { credentials: 'include', method: 'delete' })
+            .then(function () {
+                document.querySelector("#title").innerHTML = 'Du loggede af...';
+                setTimeout(function () { location.href = "/"; }, 1000);
+            });
     }
 
     // Interval-functtion der holder øje med om session-cookien stadig eksisterer
---
+++ my-cms/endpointhandlers/useritems.js
@@ -0,0 +1,70 @@
+const helpers = require('./../helpers');
+const database = require('./../data/database');
+
+module.exports = {
+    // Henter alle menupunkter fra databasen og sender dem retur
+    'GET': function (req, res) {
+        var params = helpers.getQueryParams(req);
+        var sql = "SELECT * FROM users ORDER BY id";
+        database.menuselect(res, sql, function (data) {
+            helpers.respond(res, data);
+        });
+    },
+
+    'POST': function (req, res) {
+        helpers.getFormData(req, res, function (formData) {
+            if (helpers.objEmpty(formData)) {
+                helpers.respond(res, { besked: "Der opstod en fejl" }, 500)
+                return;
+            }
+            var sql = "INSERT INTO users (username, password) values(?, ?)"
+            var values = [formData.username, formData.userpw];
+            database.query(res, sql, values, function (data) {
+                helpers.respond(res, data);
+            })
+        });
+    },
+
+    'DELETE': function (req, res) {
+        helpers.getFormData(req, res, function (formData) {
+            if (helpers.objEmpty(formData)) {
+                helpers.respond(res, { besked: 'Der opstod en fejl' }, 500);
+                return;
+            }
+            if (values == 1) {
+                alert("Du kan ikke slette denne bruger");
+            }
+            var sql = "DELETE FROM users WHERE id = ?";
+            var values = [formData.id];
+            database.query(res, sql, values, function (data) {
+                helpers.respond(res, data);
+            });
+
+        });
+    },
+
+    // Opdaterer et menupunkts 'name' og/eller 'position' i databasen
+    'PUT': function (req, res) {
+
+        var cookie = helpers.getCookies(req)  // Først hentes browserens cookie fra det indkommende request... 
+        database.verifySession(res, cookie, function (data) {   // ...dernæst verificeres om sessionen er gyldig.
+            if (helpers.objEmpty(data)) {
+                // Hvis vi er her, er der ikke fundet en gylig session i databasen der matcher browserens cookie...
+                helpers.redirect(res, '/'); // ...derfor redirectes brugeren tilbage til '/'
+                return  // og afslutter kommunikationen.
+            }
+            // Hvis vi er her er det fordi der er fundet en gyldig session i databasen der matcher browserens cookie
+            helpers.getFormData(req, res, function (formData) {  // Hent data fra formen
+                if (helpers.objEmpty(formData)) { // Check om der er nogle data
+                    helpers.respond(res, { besked: 'Der opstod en fejl' })    // Hvis ikke, så send fejlbesked.
+                }
+
+                var values = [formData.username, formData.userpw, formData.id]
+                var sql = "UPDATE users SET username = ?, password = ? WHERE id = ?";
+                database.query(res, sql, values, function (data) {
+                    helpers.respond(res, data);
+                });
+            });
+        });
+    }
+}
